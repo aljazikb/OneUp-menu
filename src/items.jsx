a5cca0f0ac87fdd1360c59e2748f226ba7ce133e
Aljazi <aljazi.kb@gmail.com>
2026-03-08 05:06:13 +0300

    first commit

---
+++ src/items.jsx
@@ -0,0 +1,53 @@
+import IMG_3033 from "./photo/IMG_3033.PNG"
+import IMG_3034 from "./photo/IMG_3034.PNG"
+import IMG_3035 from "./photo/IMG_3035.PNG"
+import IMG_3036 from "./photo/IMG_3036.PNG"
+import IMG_3037 from "./photo/IMG_3037.PNG"
+import IMG_3039 from "./photo/IMG_3039.PNG"
+
+const items = [
+    {
+        id: 1,
+        name: "Chicken Fsjita",
+        desc: "BBQ sauce with seasoned grilled chicken",
+        img: IMG_3033,
+        price: 12.5
+    },
+    {
+        id: 2,
+        name: "Chicken Mushtoom",
+        desc: "Italian sausage, ham, black olive",
+        img: IMG_3034,
+        price: 14.5
+    },
+    {
+        id: 3,
+        name: "Chicken Noodles",
+        desc: "Bacon, ham sausage",
+        img: IMG_3035 ,
+        price: 12.5
+    },
+    {
+        id: 4,
+        name: "Pasta Salmon",
+        desc: "Sliced mushrooms, green pepper, red onion, tomato and pizza mozzarella",
+        img: IMG_3036,
+        price: 11.5
+    },
+    {
+        id: 5,
+        name: "Chicken Peanut Buttor",
+        desc: "Italian sausage, pepperoni, sliced mushroom",
+        img:IMG_3037,
+        price: 14.5
+    },
+    {
+        id: 6,
+        name: "Pepperoni",
+        desc: "Double pepperoni and extra pizza mozzarella.",
+        img: IMG_3039,
+        price: 9.99
+    },
+];
+
+export default items;
